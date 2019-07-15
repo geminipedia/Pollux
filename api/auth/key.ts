de6dd5abb47b6123ccfbc7a0928f42fa1e6a23fa
Aries Cs <aries0d0f@gmail.com>
2019-07-15 17:37:37 +0800

    [Fix] Null options argument not handle.

--- api/auth/key.ts
+++ api/auth/key.ts
@@ -6,12 +6,12 @@ import { UserSignPayload, UserKeyOptions } from '../types';
 const publicKey = fs.readFileSync(path.resolve(__dirname, '../../', process.env.PUBLIC_KEY));
 const privateKey = fs.readFileSync(path.resolve(__dirname, '../../', process.env.PRIVATE_KEY));
 
-const sign = async (payload: UserSignPayload, options?: UserKeyOptions): Promise<string> => {
-  return await jwt.sign(payload, publicKey, { algorithm: 'ES512', expiresIn: options.remember ? '30d' : '1d' });
+const sign = async (payload: UserSignPayload, options: UserKeyOptions = {}): Promise<string> => {
+  return await jwt.sign(payload, privateKey, { algorithm: 'ES512', expiresIn: options.remember || false ? '30d' : '1d' });
 };
 
 const verify = async (token: string): Promise<string | object> => {
-  return await jwt.verify(token, privateKey, { algorithms: ['ES512'] });
+  return await jwt.verify(token, publicKey, { algorithms: ['ES512'] });
 };
 
 export {
