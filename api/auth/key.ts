2f668ab44acbe4b61bdbfa3229526fb11f90cf91
Aries Cs <aries0d0f@gmail.com>
2019-07-15 17:47:29 +0800

    [Fix] Correct keypair format.

--- api/auth/key.ts
+++ api/auth/key.ts
@@ -7,11 +7,11 @@ const publicKey = fs.readFileSync(path.resolve(__dirname, '../../', process.env.
 const privateKey = fs.readFileSync(path.resolve(__dirname, '../../', process.env.PRIVATE_KEY));
 
 const sign = async (payload: UserSignPayload, options: UserKeyOptions = {}): Promise<string> => {
-  return await jwt.sign(payload, privateKey, { algorithm: 'ES512', expiresIn: options.remember || false ? '30d' : '1d' });
+  return await jwt.sign(payload, privateKey, { algorithm: 'RS256', expiresIn: options.remember || false ? '30d' : '1d' });
 };
 
 const verify = async (token: string): Promise<string | object> => {
-  return await jwt.verify(token, publicKey, { algorithms: ['ES512'] });
+  return await jwt.verify(token, publicKey, { algorithms: ['RS256'] });
 };
 
 export {
