b650d66b1eb957094aa8223d4ba10988f98a8bd5
Aries Cs <aries0d0f@gmail.com>
2019-07-15 19:25:31 +0800

    [Method] Add error handler and log tracker for `Item` queris.

--- api/query/item.ts
+++ api/query/item.ts
@@ -1,24 +1,57 @@
 import { Context } from 'graphql-yoga/dist/types';
-import { Item, ItemWhereUniqueInput, ItemWhereInput } from '../model';
+import { prisma, Item, ItemWhereUniqueInput, ItemWhereInput, ItemOrderByInput, User } from '../model';
+import log from '../util/log';
+import auth from '../auth';
 
 const itemQuery = {
   async item(_: any, args: ItemWhereUniqueInput, context: Context): Promise<Item> {
-    try {
-      const targetItem = await context.prisma.item(args);
+    const viewer: User = await auth.token.parse(context.request);
 
-      if (!targetItem) { throw new Error('#ERR_I001'); }
+    try {
+      const targetItem = await prisma.item(args);
+
+      if (!targetItem) {
+        // Write Log
+        log.warn({
+          ip: context.request.ip,
+          result: 'Item not found.',
+          userId: viewer.id
+        });
+
+        throw new Error('#ERR_I001');
+      }
 
       return targetItem;
-    } catch (err) {
-      throw new Error(err.message || '#ERR_FFFF');
+    } catch (error) {
+      // Write Log
+      log.error({
+        ip: context.request.ip,
+        result: `Unexpected Error. ${error.message}`,
+        userId: viewer.id
+      });
+
+      throw new Error(error.message || '#ERR_FFFF');
     }
   },
 
-  async items(_: any, args: ItemWhereInput, context: Context): Promise<Item[]> {
+  async items({ _, args }:
+    {
+      _: any;
+      args?: {
+        where?: ItemWhereInput;
+        orderBy?: ItemOrderByInput;
+        skip?: number;
+        after?: string;
+        before?: string;
+        first?: number;
+        last?: number;
+      };
+    }
+  ): Promise<Item[]> {
     try {
-      return await context.prisma.items(args);
-    } catch (err) {
-      throw new Error(err.message || '#ERR_FFFF');
+      return await prisma.items(args);
+    } catch (error) {
+      throw new Error(error.message || '#ERR_FFFF');
     }
   }
 };
